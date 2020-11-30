367aa6ac457b2298ca7d44758b86b61d91b510fc
elsangallo <71708891+elsangallo@users.noreply.github.com>
2020-11-30 10:49:57 +0100

    Formular

--- Medexperts/main.py
+++ Medexperts/main.py
@@ -1,19 +1,31 @@
 from flask import Flask
 app = Flask("medexperts")
+from jinja2 import Jinja2
+app = Jinja2("medexperts")
 @app.route('/startseite')
 def startseite():
     return ...
-@app.route('/kalender')
-def kalender():
-
 @app.route('/falleingang'methods=['GET', 'POST'])
 def falleingang():
-    if request.method == 'Falleingang':
-        termin = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
+    if request.method == 'POST':
+        fallnr = request.form['fallnr']
+        vornamen = request.form['vornamen']
+        nachnamen = request.form['nachnamen']
+        auftraggeber = request.form['auftraggeber']
+        datum = request.form['datum']
+        termin = request.form['termin']
+        rueckgabe_string = "Fallnummer: " + fallnr /n + "Namen: " + vornamen + " " + nachnamen /n + "Auftraggeber: " + auftraggeber /n + "Eingangsdatum: " + datum /n + "Disziplin: " + disziplin /n + "Abschlusstermin: " + termin
         return rueckgabe_string
     return render_template("index.html")
 
+@app.route('/abwesenheiten')
+def abwesenheit():
+
+@app.route('/kalender')
+def kalender():
+
+@app.route('/zuordnung')
+def zuordnung():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
